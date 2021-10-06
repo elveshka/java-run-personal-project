9c0e740a9e2bb3a4e8a5842902fafb298ffb6cfc
Etrigan Snowball <esnowbal@student.21-school.ru>
2021-10-06 15:30:47 +0300

    Add empty test class

---
+++ src/test/java/httpServer/MoviesSearchPageTest.java
@@ -0,0 +1,12 @@
+package httpServer;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+
+public class MoviesSearchPageTest {
+
+    @Test
+    public void shouldReturnTrue() {
+        assertThat(true).isTrue();
+    }
+}
