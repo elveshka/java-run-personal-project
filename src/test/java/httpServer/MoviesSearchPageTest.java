6ecf8f25274917f08384651b4ac6b62832c09ff7
Etrigan Snowball <esnowbal@student.21-school.ru>
2021-10-08 23:43:12 +0300

    Transport changes

--- src/test/java/httpServer/MoviesSearchPageTest.java
+++ src/test/java/httpServer/MoviesSearchPageTest.java
@@ -1,9 +1,29 @@
 package httpServer;
 
 import static org.assertj.core.api.Assertions.*;
+
+import httpServer.Server.Server;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class MoviesSearchPageTest {
+
+
+    @Before
+    public void generatePort() {
+        Random random = new Random(1);
+//        for (;;) {
+//            try {
+//                return new ServerSocket(port);
+//            } catch (IOException ex) {
+//                // try next port
+//            }
+//        }
+        // if the program gets here, no port in the range was found
+    }
 
     @Test
     public void shouldReturnTrue() {
